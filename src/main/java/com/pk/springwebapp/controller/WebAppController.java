0d4758034d4951a9a3d8860decba025a8577dac9
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-07-10 23:10:17 +0530

    latest code

--- src/main/java/com/pk/springwebapp/controller/WebAppController.java
+++ src/main/java/com/pk/springwebapp/controller/WebAppController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebAppController {
 	@RequestMapping("/link1")
 	public String getResponse() {
+		
 		return "response.jsp";
 	}
 }
