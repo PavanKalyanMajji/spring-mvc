48bbf89f15cac2d48455c1aa3ad39be45c7621ff
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-07-10 23:07:00 +0530

    spring mvc

---
+++ src/main/java/com/pk/springwebapp/controller/WebAppController.java
@@ -0,0 +1,12 @@
+package com.pk.springwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebAppController {
+	@RequestMapping("/link1")
+	public String getResponse() {
+		return "response.jsp";
+	}
+}
